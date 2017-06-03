c90d6dbe4818d2e57d9c1a8ac5589c1015841095
bristina <bristina.temneanu@yahoo.com>
2017-06-03 13:54:08 +0300

    add configuration

--- RNN/configure.py
+++ RNN/configure.py
@@ -1,7 +1,58 @@
-ENCODER_DECODER_TYPE = "NotesModel"
+from RNN import encode_decode_one_note, encode_decode_sequence
 
 
 class ConfigRnn():
 
-    def choose_encoder_decoder(self, value):
-        pass
+    #In this class all the hyperparameters will be set
+
+    #default configuration
+    def __init__(self):
+        self.encode_decoder = encode_decode_sequence.EncodeDecodeOneHotEncoding(encode_decode_one_note.EncodeDecodeOneNote)
+        self.learning_rate = 0.0
+        self.batch_size = 1
+        self.rnn_layer_size = 1
+        self.nr_epochs_before_start = 10
+        self.nr_training_steps = 1000
+
+    #   value: 1 - OneHotEncoding
+    def set_encoder_decoder(self, value):
+
+        try:
+            if not isinstance(value, int):
+                raise TypeError
+        except TypeError:
+                print("Error: Encoder-Decoder must be an integer.Set default encoder")
+                self.encode_decoder = encode_decode_sequence.EncodeDecodeOneHotEncoding(encode_decode_one_note.EncodeDecodeOneNote)
+                return self
+
+        if value == 0:
+            self.encode_decoder = encode_decode_sequence.EncodeDecodeOneHotEncoding(encode_decode_one_note.EncodeDecodeOneNote)
+            return self
+
+        try:
+            raise ValueError
+        except ValueError:
+                print("There is no encoder with this number.Set default encoder")
+                self.encode_decoder = encode_decode_sequence.EncodeDecodeOneHotEncoding(encode_decode_one_note.EncodeDecodeOneNote)
+                return self
+
+
+    def set_learning_rate(self, learning_rate):
+        self.learning_rate = learning_rate
+        return self
+
+    def set_batch_size(self, batch_size):
+        self.batch_size = batch_size
+        return self
+
+    def set_rnn_layer_size(self, rnn_layer_size):
+        self.rnn_layer_size = rnn_layer_size
+        return self
+
+    def set_number_training_steps(self, nr_training_steps):
+        self.nr_training_steps = nr_training_steps
+        return self
+
+    def set_early_stop_after(self, nr_epochs):
+        self.nr_epochs_before_start = nr_epochs
+        return self
